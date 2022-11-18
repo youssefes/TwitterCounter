e1e2b05be879d30872ff8a27167c235c98a5a2dc
youssefes <46077105+youssefes@users.noreply.github.com>
2022-11-19 01:27:09 +0200

    static color

--- Sources/TwitterCounter/DesignSystem/Colors/Colors.swift
+++ Sources/TwitterCounter/DesignSystem/Colors/Colors.swift
@@ -8,25 +8,15 @@
 import Foundation
 import UIKit
 
-extension DesignSystem {
-    
-    
-    enum Colors : String {
-        case Main
-        case background
-        var color : UIColor {
-            switch self {
-            case .Main:
-                return rgb(red: 3, green: 169, blue: 244)
-            case .background :
-                return rgb(red: 230, green: 246, blue: 254)
-            }
-        }
-        private func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) -> UIColor {
-            return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
-        }
+extension UIColor {
+
+    public enum twitterColor {
+        static let main = UIColor.rgb(red: 3, green: 169, blue: 244)
+        static let background = UIColor.rgb(red: 230, green: 246, blue: 254)
+    }
+
+    public static func rgb(red: CGFloat, green: CGFloat, blue: CGFloat, alpha: CGFloat = 1) -> UIColor {
+        return UIColor(red: red/255, green: green/255, blue: blue/255, alpha: alpha)
     }
     
-    
 }
-
--- Sources/TwitterCounter/TwitterCounterVC/View/TwitterCounterVC.swift
+++ Sources/TwitterCounter/TwitterCounterVC/View/TwitterCounterVC.swift
@@ -22,11 +22,11 @@ public class TwitterCounterVC: UIViewController {
     func setupView(){
         title = "Twitter character count"
         presenter = TwitterCounterPresenter()
-        postTweetBtn.backgroundColor = DesignSystem.Colors.Main.color
-        logoIcon.tintColor = DesignSystem.Colors.Main.color
-        characterType.setData(title: "Characters Typed", description: "0 / 280", backgroundColorOfTitle: DesignSystem.Colors.background.color)
+        postTweetBtn.backgroundColor = UIColor.twitterColor.main
+        logoIcon.tintColor = UIColor.twitterColor.background
+        characterType.setData(title: "Characters Typed", description: "0 / 280", backgroundColorOfTitle: UIColor.twitterColor.background)
         
-        charactersRemain.setData(title: "Characters Remaining", description: "280" , backgroundColorOfTitle: DesignSystem.Colors.background.color)
+        charactersRemain.setData(title: "Characters Remaining", description: "280" , backgroundColorOfTitle: UIColor.twitterColor.background)
     }
     
     
